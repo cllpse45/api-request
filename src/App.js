d28f575680b50064a4d1d5726d3d605c3370e7f5
cllpse45 <119014171+cllpse45@users.noreply.github.com>
2023-03-12 01:07:10 +0300

    ReactProject5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Users from "./components/Users"
+
+
+// https://jsonplaceholder.typicode.com/users
 
 function App() {
   return (
-  
+  <div className="App">
+  <Users />
+
+  </div>
 
 
-    
+
   );
 }
 
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,1 +1,38 @@
 import React from "react"
+import axios from "axios";
+
+function Users(){
+    
+    //useState
+    const [data, setData] = React.useState([]);
+
+
+    async function getUsers(){
+        const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+        
+        setData(response.data);
+        console.log(response.data);
+    }
+
+    getUsers();
+
+    return(
+        <div>
+            <ul>
+                {
+                    data?.map((item) => (
+                        <>
+                        <li>{item.name}</li>
+                        <li>{item.email}</li>
+                        <li>{item.username}</li>
+                        </>
+                    ))
+                }
+            </ul>
+
+        </div>
+
+    );
+}
+
+export default Users;
